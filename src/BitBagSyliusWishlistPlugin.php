176316f16593ab206ca45768d3d59b9770c5b27a
Patryk <drapikpatryk@gmail.com>
2018-04-12 15:23:31 +0200

    first commit

---
+++ src/BitBagSyliusWishlistPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class BitBagSyliusWishlistPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
---
+++ src/DependencyInjection/BitBagSyliusWishlistExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+final class BitBagSyliusWishlistExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container): void
+    {
+    }
+}
