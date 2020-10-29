4ee21fc7e80b248b8cddd7fed69044af7dbd8695
pptasinski <przemyslaw.ptasinski@gmail.com>
2020-10-29 19:21:06 +0100

    update to sylius 1.8

--- src/Entity/WishlistTokenInterface.php
+++ src/Entity/WishlistTokenInterface.php
@@ -6,8 +6,6 @@ namespace BitBag\SyliusWishlistPlugin\Entity;
 
 interface WishlistTokenInterface
 {
-    const VALUE_LENGTH = 50;
-
     public function getValue(): string;
 
     public function setValue(string $value): void;
