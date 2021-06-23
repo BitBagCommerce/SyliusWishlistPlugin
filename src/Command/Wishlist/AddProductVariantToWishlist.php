2cdc41d363d718b187a4a021a81a05e686924b43
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-06-23 16:45:46 +0200

    Add AddProductVariantToWishlist command

---
+++ src/Command/Wishlist/AddProductVariantToWishlist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\Command\Wishlist;
+
+final class AddProductVariantToWishlist implements WishlistTokenValueAwareInterface
+{
+    public int $productVariant;
+
+    private string $token;
+
+    public function __construct(int $product)
+    {
+        $this->product = $product;
+    }
+
+    public function getWishlistTokenValue(): string
+    {
+        return $this->token;
+    }
+
+    public function setWishListTokenValue(string $token): void
+    {
+        $this->token = $token;
+    }
+
+}
