5ca698e15dea7479ccfcc665df01dc76e54b677d
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-06-23 13:01:08 +0200

    Add dispatching RemoveProductFromWishlist command in controller

--- src/Controller/Action/ApiPlatform/RemoveProductFromWishlistAction.php
+++ src/Controller/Action/ApiPlatform/RemoveProductFromWishlistAction.php
@@ -4,17 +4,28 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusWishlistPlugin\Controller\Action\ApiPlatform;
 
+use BitBag\SyliusWishlistPlugin\Command\Wishlist\RemoveProductFromWishlist;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 final class RemoveProductFromWishlistAction
 {
-    public function __construct()
+    private MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
     {
+        $this->messageBus = $messageBus;
     }
 
     public function __invoke(Request $request): JsonResponse
     {
+        $wishlistToken = $request->attributes->get('id');
+        $productId = (int)$request->attributes->get('productId');
+
+        $removeProductFromWishlist = new RemoveProductFromWishlist($productId, $wishlistToken);
+        $this->messageBus->dispatch($removeProductFromWishlist);
+
         return new JsonResponse([], 204);
     }
 }
