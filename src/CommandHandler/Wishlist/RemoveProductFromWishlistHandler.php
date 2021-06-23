2439ec6548a6729f19294ecb3ce53140e8096133
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-06-23 13:29:07 +0200

    Add removing Product from Wishlist logic in RemoveProductFromWishlistHandler

--- src/CommandHandler/Wishlist/RemoveProductFromWishlistHandler.php
+++ src/CommandHandler/Wishlist/RemoveProductFromWishlistHandler.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace BitBag\SyliusWishlistPlugin\CommandHandler\Wishlist;
 
 use BitBag\SyliusWishlistPlugin\Command\Wishlist\RemoveProductFromWishlist;
-use BitBag\SyliusWishlistPlugin\Entity\WishlistInterface;
 use BitBag\SyliusWishlistPlugin\Repository\WishlistRepositoryInterface;
+use Doctrine\Persistence\ObjectManager;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class RemoveProductFromWishlistHandler implements MessageHandlerInterface
@@ -16,16 +17,34 @@ final class RemoveProductFromWishlistHandler implements MessageHandlerInterface
 
     private WishlistRepositoryInterface $wishlistRepository;
 
+    private ObjectManager $wishlistProductManager;
+
     public function __construct(
         ProductRepositoryInterface $productRepository,
-        WishlistRepositoryInterface $wishlistRepository)
+        WishlistRepositoryInterface $wishlistRepository,
+        ObjectManager $wishlistProductManager
+    )
     {
         $this->productRepository = $productRepository;
         $this->wishlistRepository = $wishlistRepository;
+        $this->wishlistProductManager = $wishlistProductManager;
     }
 
-    public function __invoke(RemoveProductFromWishlist $removeProductFromWishlist): WishlistInterface
+    public function __invoke(RemoveProductFromWishlist $removeProductFromWishlist)
     {
+        $product = $this->productRepository->find($removeProductFromWishlist->getProductId());
+        $wishlist = $this->wishlistRepository->findByToken($removeProductFromWishlist->getWishlistToken());
 
+        if (null === $product || null === $wishlist) {
+            throw new NotFoundHttpException();
+        }
+
+        foreach ($wishlist->getWishlistProducts() as $wishlistProduct) {
+            if ($product === $wishlistProduct->getProduct()) {
+                $this->wishlistProductManager->remove($wishlistProduct);
+            }
+        }
+
+        $this->wishlistProductManager->flush();
     }
 }
