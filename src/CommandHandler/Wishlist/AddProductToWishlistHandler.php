e2299729bfc963d8481cc6c47ee681601fd10528
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-06-24 10:32:33 +0200

    Remove creating wishlist if it does not exist in AddProductToWishlistHandler

--- src/CommandHandler/Wishlist/AddProductToWishlistHandler.php
+++ src/CommandHandler/Wishlist/AddProductToWishlistHandler.php
@@ -7,6 +7,7 @@ namespace BitBag\SyliusWishlistPlugin\CommandHandler\Wishlist;
 use BitBag\SyliusWishlistPlugin\Command\Wishlist\AddProductToWishlist;
 use BitBag\SyliusWishlistPlugin\Entity\WishlistInterface;
 use BitBag\SyliusWishlistPlugin\Factory\WishlistProductFactoryInterface;
+use BitBag\SyliusWishlistPlugin\Repository\WishlistRepositoryInterface;
 use BitBag\SyliusWishlistPlugin\Resolver\AnonymousWishlistResolverInterface;
 use BitBag\SyliusWishlistPlugin\Resolver\ShopUserWishlistResolverInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -30,41 +31,33 @@ class AddProductToWishlistHandler implements MessageHandlerInterface
 
     private ProductRepositoryInterface $productRepository;
 
+    private WishlistRepositoryInterface $wishlistRepository;
+
     public function __construct(
         TokenStorageInterface $tokenStorage,
-        ShopUserWishlistResolverInterface $shopUserWishlistResolver,
-        AnonymousWishlistResolverInterface $anonymousWishlistResolver,
         WishlistProductFactoryInterface $wishlistProductFactory,
+        WishlistRepositoryInterface $wishlistRepository,
         ObjectManager $wishlistManager,
         ProductRepositoryInterface $productRepository
     ) {
         $this->tokenStorage = $tokenStorage;
-        $this->shopUserWishlistResolver = $shopUserWishlistResolver;
-        $this->anonymousWishlistResolver = $anonymousWishlistResolver;
         $this->wishlistProductFactory = $wishlistProductFactory;
         $this->wishlistManager = $wishlistManager;
         $this->productRepository = $productRepository;
+        $this->wishlistRepository = $wishlistRepository;
     }
 
     public function __invoke(AddProductToWishlist $addProductToWishlist): WishlistInterface
     {
-        $token = $this->tokenStorage->getToken();
-        $user = $token ? $token->getUser() : null;
-
         $product = $this->productRepository->find($addProductToWishlist->product);
-        if (!$product) {
+        $wishlist = $this->wishlistRepository->findByToken($addProductToWishlist->getWishlistTokenValue());
+
+        if (!$product && !$wishlist) {
             throw new NotFoundHttpException();
         }
 
-        if ($user instanceof ShopUserInterface) {
-            $wishlist = $this->shopUserWishlistResolver->resolve($user);
-        } else {
-            $wishlistToken = $addProductToWishlist->getWishlistTokenValue();
-
-            $wishlist = $this->anonymousWishlistResolver->resolve($wishlistToken);
-        }
-
         $wishlistProduct = $this->wishlistProductFactory->createForWishlistAndProduct($wishlist, $product);
+
         $wishlist->addWishlistProduct($wishlistProduct);
 
         $this->wishlistManager->persist($wishlist);
