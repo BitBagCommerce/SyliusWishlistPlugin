2e77bffe2607715e6f01f32168d74907faf7c866
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-06-28 08:32:56 +0200

    Add RemoveProductVariantFromWishlistAction controller

---
+++ src/Controller/Action/ApiPlatform/RemoveProductVariantFromWishlistAction.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\Controller\Action\ApiPlatform;
+
+use BitBag\SyliusWishlistPlugin\Command\Wishlist\RemoveProductVariantFromWishlist;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class RemoveProductVariantFromWishlistAction
+{
+    private MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $wishlistToken = (string)$request->attributes->get('id');
+        $variantId = (int)$request->attributes->get('productVariantId');
+
+        $removeProductVariantFromWishlist = new RemoveProductVariantFromWishlist($variantId, $wishlistToken);
+        $this->messageBus->dispatch($removeProductVariantFromWishlist);
+
+        return new JsonResponse([], 204);
+    }
+}
