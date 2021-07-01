5492a5c639c9d74817aa093f8807f45d1b3d9bcc
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-07-01 12:14:45 +0200

    Change WishlistProductInterface to ProductInterface in removing Product from Wishlist

--- src/Updater/WishlistUpdater.php
+++ src/Updater/WishlistUpdater.php
@@ -7,6 +7,7 @@ namespace BitBag\SyliusWishlistPlugin\Updater;
 use BitBag\SyliusWishlistPlugin\Entity\WishlistInterface;
 use BitBag\SyliusWishlistPlugin\Entity\WishlistProductInterface;
 use Doctrine\Persistence\ObjectManager;
+use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 
 final class WishlistUpdater implements WishlistUpdaterInterface
@@ -43,7 +44,7 @@ final class WishlistUpdater implements WishlistUpdaterInterface
         return $wishlist;
     }
 
-    public function removeProductFromWishlist(WishlistInterface $wishlist, WishlistProductInterface $product): WishlistInterface
+    public function removeProductFromWishlist(WishlistInterface $wishlist, ProductInterface $product): WishlistInterface
     {
         foreach ($wishlist->getWishlistProducts() as $wishlistProduct) {
             if ($product === $wishlistProduct->getProduct()) {
--- src/Updater/WishlistUpdaterInterface.php
+++ src/Updater/WishlistUpdaterInterface.php
@@ -6,6 +6,7 @@ namespace BitBag\SyliusWishlistPlugin\Updater;
 
 use BitBag\SyliusWishlistPlugin\Entity\WishlistInterface;
 use BitBag\SyliusWishlistPlugin\Entity\WishlistProductInterface;
+use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 
 interface WishlistUpdaterInterface
@@ -16,7 +17,7 @@ interface WishlistUpdaterInterface
 
     public function addProductToWishlist(WishlistInterface $wishlist, WishlistProductInterface $product): WishlistInterface;
 
-    public function removeProductFromWishlist(WishlistInterface $wishlist, WishlistProductInterface $product): WishlistInterface;
+    public function removeProductFromWishlist(WishlistInterface $wishlist, ProductInterface $product): WishlistInterface;
 
     public function removeProductVariantFromWishlist(WishlistInterface $wishlist, ProductVariantInterface $variant): WishlistInterface;
 }
