233db146b3ec9e29fde0c15afc913b41740b3329
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2021-07-13 15:51:02 +0200

    Add WishlistNotFoundException class

---
+++ src/Exception/WishlistNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\Exception;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+final class WishlistNotFoundException extends NotFoundHttpException
+{
+
+}
