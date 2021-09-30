5da3a8f3a69937681b75c279d3929df79f41bcaa
JakubSzczerba <jakszczerb@gmail.com>
2021-09-30 11:48:23 +0200

    new action

---
+++ src/Controller/Action/CreateNewWishlistAction.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\Controller\Action;
+
+use BitBag\SyliusWishlistPlugin\Entity\Wishlist;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final class CreateNewWishlistAction
+{
+    private ObjectManager $wishlistManager;
+
+    private FlashBagInterface $flashBag;
+
+    private TranslatorInterface $translator;
+
+    private UrlGeneratorInterface $urlGenerator;
+
+
+    public function __construct(
+        ObjectManager $wishlistManager,
+        FlashBagInterface $flashBag,
+        TranslatorInterface $translator,
+        UrlGeneratorInterface $urlGenerator
+
+    ) {
+        $this->wishlistManager = $wishlistManager;
+        $this->flashBag = $flashBag;
+        $this->translator = $translator;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $wishlist = new Wishlist();
+        $this->wishlistManager->persist($wishlist);
+
+        $this->wishlistManager->flush();
+        $this->flashBag->add('success', $this->translator->trans('bitbag_sylius_wishlist_plugin.ui.create_new_wishlist'));
+        return new RedirectResponse($this->urlGenerator->generate('bitbag_sylius_wishlist_plugin_shop_wishlist_list_products'));
+    }
+
+}
