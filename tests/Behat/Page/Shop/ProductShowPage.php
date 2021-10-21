9939fad1497cfbd81597683fb6056a4db24f4493
myCreedo <mycreedo98@gmail.com>
2021-10-21 12:40:20 +0200

    Fix for AddVariantToWishlist test

--- tests/Behat/Page/Shop/ProductShowPage.php
+++ tests/Behat/Page/Shop/ProductShowPage.php
@@ -19,6 +19,6 @@ class ProductShowPage extends ShowPage implements ProductShowPageInterface
         $this->getDocument()->find('css', '[data-test-wishlist-add-variant]')->click();
 
         // Wait for the ajax request to finish
-        $this->getSession()->wait(1000, 'typeof jQuery !== "undefined" && 0 === jQuery.active');
+        $this->getSession()->wait(5000, 'document.querySelectorAll("[data-test-flash-messages]").length > 0');
     }
 }
