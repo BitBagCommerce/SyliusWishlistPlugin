0bd9ee17748872eca9dbb6051f1e0981e16fe30f
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2021-10-29 06:48:14 +0200

    refactor ExportWishlistToPdfAction $ add validation in resolver $ add comments

--- src/Resolver/VariantImagePathResolver.php
+++ src/Resolver/VariantImagePathResolver.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusWishlistPlugin\Resolver;
@@ -10,7 +16,12 @@ final class VariantImagePathResolver implements VariantImagePathResolverInterfac
 {
     public function resolve(ProductVariantInterface $variant, string $baseUrl)
     {
+        if ( false === $variant->getProduct()->getImages()->first()) {
+            return 'http://placehold.it/150x150';
+        }
+
         $imagePath = $variant->getProduct()->getImages()->first()->getPath();
+
         return $baseUrl.'/media/image/'.$imagePath;
     }
 }
