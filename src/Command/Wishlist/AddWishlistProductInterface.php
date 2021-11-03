fd61a5b448e048db67c1da5448f62049d0729831
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2021-11-03 12:51:59 +0100

    add tests for ExportWishlistToPdfAction & tests for ExporterWishlistToPdf & refactor code

---
+++ src/Command/Wishlist/AddWishlistProductInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\Command\Wishlist;
+
+use BitBag\SyliusWishlistPlugin\Entity\WishlistProductInterface;
+use Sylius\Bundle\OrderBundle\Controller\AddToCartCommandInterface;
+
+interface AddWishlistProductInterface
+{
+    public function getWishlistProduct(): WishlistProductInterface;
+
+    public function setWishlistProduct(WishlistProductInterface $wishlistProduct): void;
+
+    public function isSelected(): bool;
+
+    public function setSelected(bool $selected): void;
+
+    public function getCartItem(): AddToCartCommandInterface;
+
+    public function setCartItem(AddToCartCommandInterface $cartItem): void;
+}
