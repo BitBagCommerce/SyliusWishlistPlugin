e00292c43e711bc77fd1bc119324ecb2ea8ca776
Miłosz Woźniak <51799613+milwoz@users.noreply.github.com>
2022-02-15 10:32:54 +0100

    Update CreateNewWishlistAction.php

--- src/Controller/Action/CreateNewWishlistAction.php
+++ src/Controller/Action/CreateNewWishlistAction.php
@@ -11,10 +11,8 @@ declare(strict_types=1);
 namespace BitBag\SyliusWishlistPlugin\Controller\Action;
 
 use BitBag\SyliusWishlistPlugin\Command\Wishlist\CreateNewWishlist;
-use BitBag\SyliusWishlistPlugin\Form\Type\CreateNewWishlistType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
