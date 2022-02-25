0cc4c40ff80e669b46faeb00a14e74014d18140b
milwoz <milwoz95@gmail.com>
2022-02-25 10:59:56 +0100

    Fixed javascript for copying products

--- src/Resources/assets/shop/js/handleCopyToWishlistListModal.js
+++ src/Resources/assets/shop/js/handleCopyToWishlistListModal.js
@@ -1,36 +1,36 @@
 import { CreateCopyToWishlistsListModal } from './copyToWishlistsListModal'
 
 const copyToWishlistBtn = document.querySelector('[data-bb-wl-list-modal-target="choose-wishlist-button"]')
-
+console.log(copyToWishlistBtn);
 const setAddWishlistModal = () => {
     copyToWishlistBtn.addEventListener('click', (e) => {
         e.preventDefault();
+
         new CreateCopyToWishlistsListModal(
             {},
             {
                 cancelAction: () => {},
-                performAction: async (current, target) => {
-                    const form = e.target.closest('form');
+                performAction: async () => {
+                    const form = document.querySelector('#wishlist_form');
                     const formData = new FormData(form);
-                    const wishlistsBtn = document.querySelector('[href="/wishlists"]');
-                    const url = '/wishlist/' + current + '/copy/' + target;
-                    
-                    const csrfToken = document.querySelector("[data-bb-csrf]").dataset.bbCsrf
-                    
+                    const wishlistsBtn = document.querySelector('[data-bb-wishlist-enter]');
+                    const chosenWishlist = document.querySelector('.copy-confirmation-modal select').value;
+                    const url = `${window.location.href}/copy/${chosenWishlist}`;
+                    const csrfToken = document.querySelector("[data-bb-csrf]").dataset.bbCsrf;
                     const headers = new Headers({
                         'X-CSRF-TOKEN': csrfToken
                     });
 
                     const requestConfig = {
                         method: 'POST',
-                        headers: headers, 
+                        headers: headers,
                         body: formData
                     };
 
                     try {
                         const response = await fetch(url, requestConfig);
                         const data = await response.json();
-
+                        console.log(data);
                         wishlistsBtn.classList.add('bb-copy-to-wishlist-sukces')
                         setTimeout(() => {
                             wishlistsBtn.classList.remove('bb-copy-to-wishlist-sukces')
@@ -42,13 +42,13 @@ const setAddWishlistModal = () => {
                         }, 900);
                         console.error(error);
                     } finally {
-                        
+
                     }
                 },
             }
         ).init();
     });
-    
+
 };
 
 const turnOnListener = () => {
