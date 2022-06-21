0aebe1235073799ce29c953d9b8569cec0a64bb0
piotr <piotr.szymanski@bitbag.pl>
2022-06-21 11:56:03 +0200

    add additioman mime type to validation

--- src/Form/Type/ImportWishlistFromCsvType.php
+++ src/Form/Type/ImportWishlistFromCsvType.php
@@ -54,6 +54,7 @@ final class ImportWishlistFromCsvType extends AbstractType
             'allowedMimeTypes' => [
                 'text/plain',
                 'text/csv',
+                'application/csv'
             ],
             'wishlists' => [],
         ]);
