3d6263ff4f869745515c3702d86e543d1fc85411
milwoz <milwoz95@gmail.com>
2022-08-01 12:28:34 +0200

    Removed flashbag from handler

--- src/CommandHandler/Wishlist/AddSelectedProductsToCartHandler.php
+++ src/CommandHandler/Wishlist/AddSelectedProductsToCartHandler.php
@@ -10,46 +10,37 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusWishlistPlugin\CommandHandler\Wishlist;
 
-use BitBag\SyliusWishlistPlugin\Checker\ProductCanBeProcessedCheckerInterface;
+use BitBag\SyliusWishlistPlugin\Checker\ProductProcessingChecker;
+use BitBag\SyliusWishlistPlugin\Checker\ProductProcessingCheckerInterface;
 use BitBag\SyliusWishlistPlugin\Command\Wishlist\AddSelectedProductsToCart;
 use BitBag\SyliusWishlistPlugin\Command\Wishlist\WishlistItem;
+use BitBag\SyliusWishlistPlugin\Exception\ProductCantBeAddedToCartException;
 use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
-use Sylius\Component\Order\Model\OrderItemInterface;
 use Sylius\Component\Order\Modifier\OrderItemQuantityModifierInterface;
 use Sylius\Component\Order\Modifier\OrderModifierInterface;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class AddSelectedProductsToCartHandler implements MessageHandlerInterface
 {
-    private FlashBagInterface $flashBag;
-
-    private TranslatorInterface $translator;
-
     private OrderItemQuantityModifierInterface $itemQuantityModifier;
 
     private OrderModifierInterface $orderModifier;
 
     private OrderRepositoryInterface $orderRepository;
 
-    private ProductCanBeProcessedCheckerInterface $productCanBeProcessedChecker;
+    private ProductProcessingCheckerInterface $productProcessingChecker;
 
     public function __construct(
-        FlashBagInterface $flashBag,
-        TranslatorInterface $translator,
         OrderItemQuantityModifierInterface $itemQuantityModifier,
         OrderModifierInterface $orderModifier,
         OrderRepositoryInterface $orderRepository,
-        ProductCanBeProcessedCheckerInterface $productCanBeProcessedChecker
+        ProductProcessingCheckerInterface $productProcessingChecker
     ) {
-        $this->flashBag = $flashBag;
-        $this->translator = $translator;
         $this->itemQuantityModifier = $itemQuantityModifier;
         $this->orderModifier = $orderModifier;
         $this->orderRepository = $orderRepository;
-        $this->productCanBeProcessedChecker = $productCanBeProcessedChecker;
+        $this->productProcessingChecker = $productProcessingChecker;
     }
 
     public function __invoke(AddSelectedProductsToCart $addSelectedProductsToCartCommand): void
@@ -61,8 +52,10 @@ final class AddSelectedProductsToCartHandler implements MessageHandlerInterface
     {
         /** @var WishlistItem $wishlistProduct */
         foreach ($wishlistProducts as $wishlistProduct) {
-            if ($this->productCanBeProcessedChecker->productCanBeProcessed($wishlistProduct)) {
+            if ($this->productProcessingChecker->productCanBeProcessed($wishlistProduct)) {
                 $this->addProductToWishlist($wishlistProduct);
+            } else {
+                throw new ProductCantBeAddedToCartException();
             }
         }
     }
@@ -78,10 +71,5 @@ final class AddSelectedProductsToCartHandler implements MessageHandlerInterface
 
         $this->orderModifier->addToOrder($cart, $cartItem);
         $this->orderRepository->add($cart);
-
-        if (false === $this->flashBag->has('success')){
-            $this->flashBag->add('success', $this->translator->trans('bitbag_sylius_wishlist_plugin.ui.added_to_cart'));
-        }
-
     }
 }
