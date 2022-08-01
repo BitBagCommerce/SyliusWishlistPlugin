b8b501b8b40529374e53cb0bc10fa9f5d39c2ca1
milwoz <milwoz95@gmail.com>
2022-08-01 12:29:04 +0200

    Added flashbag to controller

--- src/Controller/Action/AddSelectedProductsToCartAction.php
+++ src/Controller/Action/AddSelectedProductsToCartAction.php
@@ -11,13 +11,53 @@ declare(strict_types=1);
 namespace BitBag\SyliusWishlistPlugin\Controller\Action;
 
 use BitBag\SyliusWishlistPlugin\Command\Wishlist\AddSelectedProductsToCart;
+use BitBag\SyliusWishlistPlugin\Exception\ProductCantBeAddedToCartException;
+use BitBag\SyliusWishlistPlugin\Processor\WishlistCommandProcessorInterface;
+use BitBag\SyliusWishlistPlugin\Repository\WishlistRepositoryInterface;
+use Sylius\Component\Order\Context\CartContextInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class AddSelectedProductsToCartAction extends BaseWishlistProductsAction
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(
+        CartContextInterface $cartContext,
+        FormFactoryInterface $formFactory,
+        FlashBagInterface $flashBag,
+        WishlistCommandProcessorInterface $wishlistCommandProcessor,
+        MessageBusInterface $messageBus,
+        UrlGeneratorInterface $urlGenerator,
+        WishlistRepositoryInterface $wishlistRepository,
+        TranslatorInterface $translator
+    ) {
+        parent::__construct(
+            $cartContext,
+            $formFactory,
+            $flashBag,
+            $wishlistCommandProcessor,
+            $messageBus,
+            $urlGenerator,
+            $wishlistRepository
+        );
+        $this->translator = $translator;
+    }
+
     protected function handleCommand(FormInterface $form): void
     {
-        $command = new AddSelectedProductsToCart($form->getData());
-        $this->messageBus->dispatch($command);
+        try {
+            $command = new AddSelectedProductsToCart($form->getData());
+            $this->messageBus->dispatch($command);
+            $this->flashBag->add('success', $this->translator->trans('bitbag_sylius_wishlist_plugin.ui.added_to_cart'));
+        } catch (HandlerFailedException $exception) {
+            $exception->getNestedExceptionOfClass(ProductCantBeAddedToCartException::class);
+            $this->flashBag->add('error', $this->translator->trans('bitbag_sylius_wishlist_plugin.ui.increase_quantity'));
+        }
     }
 }
