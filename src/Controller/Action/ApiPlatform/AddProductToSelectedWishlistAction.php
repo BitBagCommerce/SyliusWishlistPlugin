825683315065bd2cb4f03ff84f4b678420f11419
Ahmed Aziz Abbassi <77698923+3zeyez@users.noreply.github.com>
2025-01-13 02:10:30 +0100

    Fix: create a new ApiPlatform action to add product to wishlist

---
+++ src/Controller/Action/ApiPlatform/AddProductToSelectedWishlistAction.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file has been created by developers from Softylines.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://softylines.com and write us
+ * an email on ask@softylines.com.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusWishlistPlugin\Controller\Action\ApiPlatform;
+
+use BitBag\SyliusWishlistPlugin\Command\Wishlist\AddProductToSelectedWishlist;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class AddProductToSelectedWishlistAction
+{
+    private MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $wishlistToken = (string) $request->attributes->get('token');
+        $productId = (int) $request->attributes->get('productId');
+
+        $addProductToSelectedWishlist = new AddProductToSelectedWishlist($wishlistToken, $productId);
+        $this->messageBus->dispatch($addProductToSelectedWishlist);
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+}
