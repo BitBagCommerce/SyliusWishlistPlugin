4bdf0efb407296223b9d1a06a399c228fd5e6f27
Paweł Piórkowski <pawel.piorkowski@mailmetrics.com>
2025-01-30 15:12:09 +0100

    fix/OP-565 [Wishlist] Issue with multiple wishlists using the same token

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -3,4 +3,5 @@
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
