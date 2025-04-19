6ea8356e6ede29115920b4190a9a8d4dc92d2490
jkindly <kozupa.jakub@gmail.com>
2025-04-19 20:11:36 +0200

    HM-1: OS Header -> Sylius header

--- src/Command/RemoveGuestWishlistsCommand.php
+++ src/Command/RemoveGuestWishlistsCommand.php
@@ -1,10 +1,12 @@
 <?php
 
 /*
- * This file has been created by developers from BitBag.
- * Feel free to contact us once you face any issues or want to start
- * You can find more information about us on https://bitbag.io and write us
- * an email on hello@bitbag.io.
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 declare(strict_types=1);
@@ -19,7 +21,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class RemoveGuestWishlistsCommand extends Command
 {
-    protected static $defaultName = 'bitbag:wishlist:remove-guest-wishlists';
+    protected static $defaultName = 'sylius:wishlist:remove-guest-wishlists';
 
     public function __construct(private WishlistRepositoryInterface $wishlistRepository)
     {
